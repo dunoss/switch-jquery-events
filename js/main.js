b3f0aac9c3cc2a6a19236b9a4d35b9ef35b2a650
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-05-06 16:22:40 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+$(function() {
+  $('button.switch').on('click', function(){
+    $(this).toggleClass('on');
+    $(this).toggleClass('off');
 
+    $('body').toggleClass('light');
+    $('body').toggleClass('dark');
 
+    if ($(this).hasClass('on')) {
+      $('.status').text("Lights on");
+    } else {
+      $('.status').text("Lights off");
+    }
+  });
+});
